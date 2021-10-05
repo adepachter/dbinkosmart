9b19b4b8624662b4e98a3f6bd6ce3cecc70da22d
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-10-05 18:46:14 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd29636ecf7f37349eb1805d226e505c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'baab9bc3-0e93-48dc-8dd4-0587a50b5268'
+};
