600824fb67ed55e09e6338e5e039fad4a77395b3
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-10-10 15:44:54 +0200

    strapi ssl and nginx

--- config/server.js
+++ config/server.js
@@ -1,9 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cd29636ecf7f37349eb1805d226e505c'),
-    },
-  },
+  url: 'https://api.inkosmart.be',
 });
